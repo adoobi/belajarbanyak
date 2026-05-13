56bd1133e40c1b35ec9d9a1fb69a7e0f84a0e3e5
Dwi-Muafa-Nashrulloh <dwi.nasrulloh@digitalbdg.ac.id>
2026-05-13 13:40:27 +0700

    paseto step2

---
+++ irfan-belajarpaseto/step2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	paseto "aidanwoods.dev/go-paseto"
+)
+
+func step2() {
+
+	fmt.Println("CREATING TOKEN...")
+
+	token := paseto.NewToken()
+
+	token.SetIssuedAt(time.Now())
+	token.SetExpiration(time.Now().Add(2 * time.Hour))
+
+	token.SetString("user-id", "user-123")
+
+	// ENCRYPT TOKEN DISINI
+	// key := paseto.NewV4SymmetricKey() // don't share this!!
+	// encrypted := token.V4Encrypt(key, nil)
+
+	// ENCRYPT PUBLIC
+	secretKey := paseto.NewV4AsymmetricSecretKey() // don't share this!!!
+	publicKey := secretKey.Public()                // DO share this one
+
+	signed := token.V4Sign(secretKey, nil)
+
+	fmt.Println("\nTOKEN CREATED (not encrypted yet)")
+	fmt.Println(token)
+
+	// fmt.Println("\ENCRYPT KEY")
+	// fmt.Println(encrypted)
+
+	fmt.Println("\nPUBLIC KEY")
+	fmt.Println(publicKey)
+
+	fmt.Println("\nSIGNED TOKEN:")
+	fmt.Println(signed)
+
+}
